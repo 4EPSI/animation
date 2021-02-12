3f4bae5492e131b2cddb30d63a580022604cff6c
4EPSI <70267131+4EPSI@users.noreply.github.com>
2021-02-12 17:04:02 +0200

    gsap animate

--- js/main.js
+++ js/main.js
@@ -57,3 +57,110 @@ gsap.to(".box-4", {
     yoyo: true,
     repeatRefresh: true
   });
+
+// BOX-5
+//   repeat and yoyo OUTSIDE stagger object
+gsap.to(".box-5", {
+    y: 100,
+    duration: 0.5,
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+      from: "center",
+      each: 0.1,
+    }
+  });
+
+//   repeat and yoyo INSIDE stagger object
+//   gsap.to(".box-5", {
+//     y: 100,
+//     duration: 0.5,
+//     stagger: {
+//       from: "center",
+//       each: 0.1,
+//       repeat: -1,
+//       yoyo: true,
+//     }
+//   });
+
+// BOX-6
+gsap.to(".box-6", {
+    duration: 1,
+    scale: 0.1,
+    ease: "power1.inOut",
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+      each: 0.1,
+      from: "center",
+      grid: "auto"
+      // axis: "y"
+    }
+  });
+//   ball-1
+gsap.to('.ball-1', {
+    duration: 1.5,
+    x: 200,
+    scale: 2,
+    ease: 'bounce'
+  })
+
+  gsap.to('.ball-1', {
+    duration: 1.5,
+    delay: 1.5,
+    x: 0,
+    scale: 1,
+    ease: 'back.inOut(3)'
+  })
+  // ball-2
+  gsap
+  .timeline({
+    repeat: -1,
+    defaults: {
+      duration: 1.5
+    }
+  })
+  .to('.ball-2', {
+    x: 200,
+    scale: 2,
+    ease: "bounce"
+  })
+  .to('.ball-2', {
+    x: 0,
+    scale: 1,
+    ease: "back.inOut(3)"
+  })
+  .to('.ball-2', {
+    x: -200,
+    scale: 2,
+    ease: "bounce"
+  })
+  .to('.ball-2', {
+    x: 0,
+    scale: 1,
+    ease: "back.inOut(3)"
+  });
+
+  // bal-3
+  gsap
+  .timeline({
+    defaults: {
+      duration: 1.5
+    }
+  })
+  .to('.ball-3', {
+    x: 300,
+    scale: 2,
+    ease: "bounce"
+  })
+  .to('.ball-4', {
+    x: 300,
+    scale: 2,
+    ease: "bounce"
+  }, '-=1')
+  .to('.ball-5', {
+    x: 300,
+    scale: 2,
+    ease: "bounce"
+  }, '-=1')
+
